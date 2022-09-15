4640814b20a44c92890c80f6932f1a98cd60d70d
Josh Comer <jcomer@cvent.com>
2022-09-15 14:11:50 -0300

    Add back date/year defaults

--- aoc.clj
+++ aoc.clj
@@ -1,30 +1,40 @@
 (ns aoc
   (:require [script.runner :as runner]
             [script.generate :as generate]
-            [script.api :as api]))
+            [script.api :as api]
+            [script.util :as util]))
 
-(defn run-solution [{:keys [input-dir auth-file year day]}]
+(defn run-solution [{:keys [input-dir auth-file year day]
+                     :or {year (util/get-current-year)
+                          day (util/get-current-day)}}]
   (runner/run-solution (str auth-file)
                        (str input-dir)
                        (str year)
                        (str day)))
 
-(defn run-tests [{:keys [year day]}]
+(defn run-tests [{:keys [year day]
+                  :or {year (util/get-current-year)
+                       day (util/get-current-day)}}]
   (runner/run-tests (str year) (str day)))
 
 (defn gen-new [{:keys [year day solution-template]
-                :or {solution-template "script/solution_template.clj"}}]
+                :or {solution-template "script/solution_template.clj"
+                     year (util/get-current-year)
+                     day (util/get-current-day)}}]
   (generate/generate-solution (str solution-template)
                               (str year)
                               (str day)))
 
-(defn get-input [{:keys [auth-file input-dir year day]}]
+(defn get-input [{:keys [auth-file input-dir year day]
+                  :or {year (util/get-current-year)
+                       day (util/get-current-day)}}]
   (api/download-input (str auth-file)
                       (str input-dir)
                       (str year)
                       (str day)))
 
-(defn store-auth [{:keys [auth-file year session]}]
+(defn store-auth [{:keys [auth-file year session]
+                   :or {year (util/get-current-year)}}]
   (api/store-auth (str auth-file)
                   (str year)
                   (str session)))
